8fb757130a9221bdc3d9b36b65fa334f5ea2a4de
Naveenchandar <naveenchandece@gmail.com>
2022-08-13 16:28:27 +0530

    feat: changed addtoplaylist method logics and added new router

--- routers/playlists/helper.js
+++ routers/playlists/helper.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const { fetchModel, findAll, createItem, updateItemById, errorResponse, findItemById } = require('../../utils');
 
 const fetchPlaylistModel = async (req, res) => {
@@ -20,15 +21,50 @@ const fetchAllPlaylists = async (req, res) => {
     }
 }
 
-const addPlaylist = async (req, res) => {
+const fetchPlaylistsByUser = async (req, res, next) => {
     try {
-        const { name = '' } = req?.body;
-        if (name) {
-            return await createItem({ modelName: 'playlists', req, res });
-        }
-        throw new Error('name key is missing');
+        const { id: userId } = req.params;
+        if (!userId) throw new Error('userId key is missing from payload');
+
+        const model = await fetchModel('users');
+        const playlistModel = await fetchModel('playlists');
+        if (!model) throw { status: 404, message: `Unable to find ${model} model` };
+        if (!playlistModel) throw { status: 404, message: `Unable to find ${playlistModel} model` };
+        const findUser = await model.findById({ _id: userId });
+        if (!findUser) throw { status: 404, message: `User with userid ${userId} not found` };
+        const playlists = await playlistModel.find();
+        if (!playlists) throw { status: 404, message: `User with userid ${userId} not found` };
+        const data = playlists.filter(item => item.user?.toString() === userId);
+        res.status(200).json({ playlists: data });
     } catch (error) {
-        errorResponse({ error, type: 'addPlaylist error', res });
+        next(error);
+    }
+}
+
+const addPlaylist = async (req, res, next) => {
+    try {
+        const { name = '', userId = '', videos = [] } = req?.body;
+        if (!name) throw new Error('name key is missing from payload');
+        if (!userId) throw new Error('userId key is missing from payload');
+        const model = await fetchModel('users');
+        const playlistModel = await fetchModel('playlists');
+        if (!model) throw { status: 404, message: `Unable to find ${model} model` };
+        if (!playlistModel) throw { status: 404, message: `Unable to find ${playlistModel} model` };
+        let user = await model.findById({ _id: userId });
+        if (!user) throw { status: 404, message: `User with userid ${userId} not found` };
+        if (user?.playlists?.length > 0) {
+            const isPlaylistNamePresent = user?.playlists?.find(item => item.name === name);
+            if (isPlaylistNamePresent) throw new Error(`Playlist ${name} already exists!`);
+        }
+        const object = { name, user, videos };
+        const data = await playlistModel.create(object);
+        await user.playlists.push(data);
+        await user.save();
+        console.log('user:', user)
+        if (!data?._id) throw new Error('Playlist creation failed, Please try again');
+        res.status(201).json({ message: 'playlist created successfully', playlist: data })
+    } catch (error) {
+        next(error);
     }
 }
 
@@ -57,4 +93,11 @@ const updatePlaylistsById = async (req, res) => {
     }
 }
 
-module.exports = { fetchPlaylistModel, fetchAllPlaylists, addPlaylist, updatePlaylistsById, fetchPlaylistsById };
+module.exports = {
+    fetchPlaylistModel,
+    fetchAllPlaylists,
+    addPlaylist,
+    updatePlaylistsById,
+    fetchPlaylistsById,
+    fetchPlaylistsByUser
+};
